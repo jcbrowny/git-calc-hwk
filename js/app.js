62c56ccf356931b734dbfa0a7a074b39e0c38d47
Tech-a53 <tech-a53@Tech-A53s-MacBook-Air.local>
2019-04-15 22:54:21 +0100

    made feature 3 work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // alert("I am a calculator")
-var math_choice = prompt("Choose a function: (1)addition (2) subtraction (3)multiplication (4)Dividing");
+var math_choice = prompt("Choose a function: (1)addition (2) subtraction (3)multiplication (4)Dividing (5)Quit");
 
-if (math_choice == 1){
+if (math_choice == 1 || " "){
   var a = +prompt("Enter first number");
   var b = +prompt("Enter second number");
   alert (a + b);
@@ -17,4 +17,13 @@ if (math_choice == 1){
   var a = +prompt("Enter first number");
   var b = +prompt("Enter second number");
   alert (a / b);
+}
+else if (math_choice == 5) {
+  alert("closing calculator")
+
+}
+var restart = 0
+while (restart < 5) {
+  console.log("this has ran" + restart + " times");
+  restart++
 }
