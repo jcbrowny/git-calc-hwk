7a212b45d6692e92fc487fd5425eceb1660a77d0
Tech-a53 <tech-a53@Tech-A53s-MacBook-Air.local>
2019-04-15 22:40:50 +0100

    made first alert box

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// alert("I am a calculator")
+var math_choice = prompt("Choose a function: (1)addition (2) subtraction (3)multiplication (4)Dividing");
+
+if (math_choice == 1){
+  var a = +prompt("Enter first number");
+  var b = +prompt("Enter second number");
+  alert (a + b);
+}else if (math_choice == 2){
+  var a = +prompt("Enter first number");
+  var b = +prompt("Enter second number");
+  alert (a - b);
+}else if (math_choice == 3){
+  var a = +prompt("Enter first number");
+  var b = +prompt("Enter second number");
+  alert (a * b)
+}else if (math_choice == 4){
+  var a = +prompt("Enter first number");
+  var b = +prompt("Enter second number");
+  alert (a / b)
+}
